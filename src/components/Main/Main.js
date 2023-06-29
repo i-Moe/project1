051787f60a38907dca588c9ee16c03d4c32c26fb
i-Moe <potato3gg@gmail.com>
2023-06-29 17:42:31 +0200

    yay

---
+++ src/components/Main/Main.js
@@ -0,0 +1,17 @@
+// components/MainContent/MainContent.js
+
+import React from 'react';
+import './Main.css';
+
+function Main() {
+  return (
+    <main className="main">
+      <section>
+        <h2>Welcome to my landing page!</h2>
+        <p>Feel free to explore and learn more about what I have to offer.</p>
+      </section>
+    </main>
+  );
+}
+
+export default Main;
